10999ec5c91a8d383b85bd6f0e04029b26a7f539
haridheep10 <haridheep10@gmail.com>
2026-03-16 13:30:11 +0530

    uc2-hardcore

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker App");
-    }
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Input text: ");
+        String str = sc.nextLine();
+
+        String reversed = "";
+
+        // Reverse the string
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+
+        // Check palindrome
+        if (str.equalsIgnoreCase(reversed)) {
+            System.out.println("Is it a Palindrome? : true");
+        } else {
+            System.out.println("Is it a Palindrome? : false");
+        }
+
+        sc.close();
+    }
 }
